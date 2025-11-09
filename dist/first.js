a00fb8e08debb5b63e0578d33429c4976f8f030e
SajidSojib <sajidahmed2401@gmail.com>
2025-11-09 20:45:49 +0600

    first commit

---
+++ dist/first.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = 'John';
+const age = 30;
+console.log(name, age);
+//# sourceMappingURL=first.js.map
---
+++ src/first.ts
@@ -0,0 +1,4 @@
+const name: string = 'John';
+const age: number = 30;
+const age2: number = 30;
+console.log(name, age, age2);
